fff5e6a86246b9e0bd88cfb11a0c80f07fb5fa45
Jon Uhlmann <me@jonnitto.ch>
2021-09-20 02:34:09 +0200

    New: Fire event on switch

--- Resources/Private/Assets/Backend.js
+++ Resources/Private/Assets/Backend.js
@@ -3,16 +3,37 @@ const BACKEND_EDIT_CLASS = NAMESPACE + "--edit";
 [...document.querySelectorAll("." + NAMESPACE)].forEach((element) => {
     const BUTTON = element.querySelector(`.${NAMESPACE}__switcher`);
     const LIVE = element.querySelector(`.${NAMESPACE}__live`);
+    const type = element.dataset.type;
     if (BUTTON) {
         BUTTON.addEventListener("click", () => {
             if (element.classList.contains(BACKEND_EDIT_CLASS)) {
                 window.location.reload();
+                triggerEvent({ mode: "live", element, type });
                 return;
             }
             if (LIVE) {
-                LIVE.remove();
+                setTimeout(() => {
+                    LIVE.remove();
+                }, 10);
             }
             element.classList.add(BACKEND_EDIT_CLASS);
+            triggerEvent({ mode: "edit", element, type });
         });
     }
 });
+
+const triggerEvent = (options) => {
+    setTimeout(() => {
+        let event;
+        if (!options) {
+            options = {};
+        }
+        if (window.CustomEvent) {
+            event = new CustomEvent("carbonCBD", { detail: options });
+        } else {
+            event = document.createEvent("CustomEvent");
+            event.initCustomEvent("carbonCBD", true, true, options);
+        }
+        document.dispatchEvent(event);
+    }, 20);
+};
